8200a4da2bbe798af1728f46e8f1e638173b3d74
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2025-04-21 10:14:34 -0700

    Set up PNG mod and pass all tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod args;
 mod chunk;
 mod chunk_type;
+use std::str::FromStr;
+
+use crate::chunk_type::{ChunkType};
 mod commands;
 mod png;
 
@@ -8,5 +11,15 @@ pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
 
 fn main() -> Result<()> {
-    todo!()
+    let result = ChunkType::try_from([73, 72, 68, 82]);
+    // let result = ChunkType::from_str("Ru1t");
+
+    // println!("{:?}", "Rust".as_bytes());
+    
+    match result {
+        Ok(val) => println!("Result is: {}", val),
+        Err(e) => println!("Error: {}", e)
+    }
+
+    Ok(())
 }
